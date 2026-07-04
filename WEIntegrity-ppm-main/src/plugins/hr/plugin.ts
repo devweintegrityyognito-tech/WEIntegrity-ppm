9249b98dc616689b32d0cdad309ea3fdb60ded3a
MOHAMMAD MANSOOR PASHA <mohammadmansoor2804@gmail.com>
2026-07-04 11:18:44 +0530

    Remove duplicate WEIntegrity-ppm-main folder

--- WEIntegrity-ppm-main/src/plugins/hr/plugin.ts
+++
@@ -1,5 +0,0 @@
-export const hrPlugin = {
-    id: "hr",
-    name: "HR Module",
-    description: "Human Resource Management"
-};
--- WEIntegrity-ppm-main/src/routes/stories.index.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, Navigate } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/stories/")({
-  component: () => <Navigate to="/stories/all" replace />,
-});
